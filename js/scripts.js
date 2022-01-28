91fbb024596211cc445b2b33fcdfe5c4a307a812
S. Espinet <s.lou.espinet@gmail.com>
2022-01-28 09:33:15 -0800

    add js for test 2 (small pizza cost prototype)

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ function Pizza (toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.pizzaCost = function () {
+  let pizzaBaseCost = 10;
+  let toppingsArray = this.toppings;
+  let toppingsCost = toppingsArray.length * 1;
+  pizzaCost = toppingsCost + pizzaBaseCost;
+  return pizzaCost;
+};
