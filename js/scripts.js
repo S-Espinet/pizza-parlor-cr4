1cd902a96247083dee61f2f69f2d8fa652d2f8f6
S. Espinet <s.lou.espinet@gmail.com>
2022-01-28 15:01:03 -0800

    WIP: trying to fix undefined result

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,12 @@ function Pizza (toppings, size) {
 
 Pizza.prototype.pizzaCost = function () {
   let pizzaBaseCost = 10;
-  let toppingsArray = this.toppings;
+  //let toppingsArray = this.toppings;
+  let toppingsArray = []
   if (toppingsArray.length === 0) {
     return "Please select at least one option."
   }
-  else if (toppingsArray.length !== 0 && this.size === "small") {
+  if (toppingsArray.length !== 0 && this.size === "small") {
     return toppingsArray.length * 1 + pizzaBaseCost
   }
   else if (toppingsArray.length !== 0 && this.size === "medium") {
@@ -19,3 +20,20 @@ Pizza.prototype.pizzaCost = function () {
     return toppingsArray.length * 3 + pizzaBaseCost + 3;
 }
 };
+
+
+$(document).ready (function() {
+  console.log("Document is ready")
+  let myPizza = new Pizza()
+  let toppingsArray = [];
+  console.log(toppingsArray);
+  if ($("#cheese").is(":checked")) {
+  toppingsArray.push("#cheese");
+  console.log(toppingsArray);
+}
+  this.size = $("input:radio[name=size]:checked").val();
+  $("button#cost").click (function() {
+    myPizza.pizzaCost();
+    console.log(myPizza);
+  })
+})
